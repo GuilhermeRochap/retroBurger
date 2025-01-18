9c8133d61986f4fb7319f3af532ff33389a8a973
GuilhermeRochap <guirpessanha@gmail.com>
2025-01-18 15:49:31 -0300

    sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Sidebar } from "@/components/sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={cn("min-h-screen bg-background font-sans antialiased"
+      )}>
+        <Sidebar/>
+        {children}</body>
     </html>
   );
 }
